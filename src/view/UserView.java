8ddbe345784cb5e763c9824de9a33cb5ffa1b51d
JephyrWing <gad03302@gmail.com>
2026-04-21 09:44:12 +0900

    feat : Main과 UserView 연결 작업 완료

---
+++ src/view/UserView.java
@@ -0,0 +1,26 @@
+package view;
+
+import java.util.Scanner;
+
+public class UserView {
+    private final Scanner sc;
+
+    public UserView(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void insert() {
+    }
+
+    public void update() {
+    }
+
+    public void delete() {
+    }
+
+    public void searchAll() {
+    }
+
+    public void searchByID() {
+    }
+}
